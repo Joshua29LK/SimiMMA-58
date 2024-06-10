ccb5b3f94eb75bd0a502fd1b7fc62451a23bf64f
Bss Dev <technical@bsscommerce.com>
2024-06-10 10:57:40 +0000

    Fix bug api v1

--- app/code/Bss/ThemeDesign/Model/Config.php
+++ app/code/Bss/ThemeDesign/Model/Config.php
@@ -41,29 +41,29 @@ class Config implements ConfigInterface
     public function getAppConfig()
     {
         $themeSelected = $this->scopeConfig->getValue('themedesign/theme_list/selected_theme');
-
+        
         $themeConfig = [
             'theme_selected' => $themeSelected,
-            'key_color' => $this->getStoreConfig('themedesign/basic_colors/key_color'),
-            'top_menu_icon_color' => $this->getStoreConfig('themedesign/basic_colors/top_menu_icon_color'),
-            'button_background' => $this->getStoreConfig('themedesign/basic_colors/button_background_color'),
-            'button_text_color' => $this->getStoreConfig('themedesign/basic_colors/button_text_color'),
-            'menu_background' => $this->getStoreConfig('themedesign/basic_colors/menu_background_color'),
-            'menu_text_color' => $this->getStoreConfig('themedesign/basic_colors/menu_text_color'),
-            'menu_line_color' => $this->getStoreConfig('themedesign/basic_colors/menu_line_color'),
-            'menu_icon_color' => $this->getStoreConfig('themedesign/basic_colors/menu_icon_color'),
-            'search_box_background' => $this->getStoreConfig('themedesign/advance_colors/search_box_background_color'),
-            'search_text_color' => $this->getStoreConfig('themedesign/advance_colors/search_text_color'),
-            'app_background' => $this->getStoreConfig('themedesign/advance_colors/app_background_color'),
-            'content_color' => $this->getStoreConfig('themedesign/advance_colors/content_color'),
-            'image_border_color' => $this->getStoreConfig('themedesign/advance_colors/image_border_color'),
-            'line' => $this->getStoreConfig('themedesign/advance_colors/line_color'),
-            'price_color' => $this->getStoreConfig('themedesign/advance_colors/price_color'),
-            'special_price_color' => $this->getStoreConfig('themedesign/advance_colors/special_price_color'),
-            'icon_color' => $this->getStoreConfig('themedesign/advance_colors/icon_color'),
-            'section_color' => $this->getStoreConfig('themedesign/advance_colors/section_color'),
-            'status_bar_text' => $this->getStoreConfig('themedesign/advance_colors/status_bar_text_color'),
-            'loading_color' => $this->getStoreConfig('themedesign/advance_colors/loading_color')
+            'key_color' => $this->scopeConfig->getValue('themedesign/basic_colors/key_color'),
+            'top_menu_icon_color' => $this->scopeConfig->getValue('themedesign/basic_colors/top_menu_icon_color'),
+            'button_background' => $this->scopeConfig->getValue('themedesign/basic_colors/button_background_color'),
+            'button_text_color' => $this->scopeConfig->getValue('themedesign/basic_colors/button_text_color'),
+            'menu_background' => $this->scopeConfig->getValue('themedesign/basic_colors/menu_background_color'),
+            'menu_text_color' => $this->scopeConfig->getValue('themedesign/basic_colors/menu_text_color'),
+            'menu_line_color' => $this->scopeConfig->getValue('themedesign/basic_colors/menu_line_color'),
+            'menu_icon_color' => $this->scopeConfig->getValue('themedesign/basic_colors/menu_icon_color'),
+            'search_box_background' => $this->scopeConfig->getValue('themedesign/advance_colors/search_box_background_color'),
+            'search_text_color' => $this->scopeConfig->getValue('themedesign/advance_colors/search_text_color'),
+            'app_background' => $this->scopeConfig->getValue('themedesign/advance_colors/app_background_color'),
+            'content_color' => $this->scopeConfig->getValue('themedesign/advance_colors/content_color'),
+            'image_border_color' => $this->scopeConfig->getValue('themedesign/advance_colors/image_border_color'),
+            'line' => $this->scopeConfig->getValue('themedesign/advance_colors/line_color'),
+            'price_color' => $this->scopeConfig->getValue('themedesign/advance_colors/price_color'),
+            'special_price_color' => $this->scopeConfig->getValue('themedesign/advance_colors/special_price_color'),
+            'icon_color' => $this->scopeConfig->getValue('themedesign/advance_colors/icon_color'),
+            'section_color' => $this->scopeConfig->getValue('themedesign/advance_colors/section_color'),
+            'status_bar_text' => $this->scopeConfig->getValue('themedesign/advance_colors/status_bar_text_color'),
+            'loading_color' => $this->scopeConfig->getValue('themedesign/advance_colors/loading_color')
         ];
 
         $languagesConfig = [];
